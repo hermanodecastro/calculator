27eebd5695a3e6352ef74f386792fa7194637bdd
Hermano <srhermanodecastro@gmail.com>
2020-12-26 09:07:39 -0300

    first commit

---
+++ public/js/calculator.js
@@ -0,0 +1,99 @@
+let display = document.getElementById("display");
+
+let displayStatus = {
+	isFinalResult: false,
+	stillCalculating: true,
+	isOn: true,
+}
+
+display.value = 0;
+
+function numericButton(button) {
+	if(displayStatus.isOn) {
+		if(displayStatus.isFinalResult && !displayStatus.stillCalculating) {
+			display.value = 0;
+		}
+		if(display.value == 0 && display.value.length == 1) {
+			display.value = display.value.slice(0, -1);
+			display.value += button.innerHTML;
+			displayStatus.isFinalResult = false;
+		} else {
+			display.value += button.innerHTML;
+		}
+	}
+}
+
+function operatorButton(button) {
+	if(displayStatus.isOn) {
+		displayStatus.stillCalculating = true;
+		let lastNonEmptyIndex = display.value.length - 2;
+		let lastIndex = display.value.length - 1;
+		if(isAnOperator(display.value[lastNonEmptyIndex])) {
+			display.value = display.value.slice(0, - 3);
+			display.value += " " + button.innerHTML + " ";
+		} else if(display.value[lastIndex] == ".") {
+			display.value = display.value.slice(0, -1)
+			display.value += "0" + " " + button.innerHTML + " ";
+		} else {
+			display.value += " " + button.innerHTML + " ";
+		}
+	}
+}
+
+function dotButton(button) {
+	if(displayStatus.isOn) {
+		let lastIndex = display.value.length - 1;
+		if(display.value[lastIndex] != ".") {
+			display.value += button.innerHTML;
+		}
+	}
+}
+
+function backButton() {
+	let lastIndex = display.value.length - 1;
+	if(display.value[lastIndex] == " ") {
+		display.value = display.value.slice(0, -2);
+	} else {
+		display.value = display.value.slice(0, -1);
+	}
+}
+
+function clearButton() {
+	if(display.value != "") {
+		display.value = 0;
+	}
+}
+
+function onOffButton() {
+	if(display.value == "") {
+		display.value = 0;
+		displayStatus.isOn = true;
+	} else {
+		display.value = "";
+		displayStatus.isOn = false;
+	}
+}
+
+function equalsButton() {
+	if(display.value != "") {
+		let lastNonEmptyIndex = display.value.length - 2;
+		if(isAnOperator(display.value[lastNonEmptyIndex])) {
+			display.value = display.value.slice(0, -2);
+		}
+		console.log(display.value.replace(/[x]/g, "*"));
+		let result = eval(display.value.replace(/[x]/g, "*"));
+		display.value = result;
+		displayStatus.isFinalResult = true;
+		displayStatus.stillCalculating = false;
+	} 
+}
+
+function isAnOperator(lastCharacter) {
+	if(lastCharacter == "x" || lastCharacter == "/" || lastCharacter == "+" || lastCharacter == "-") {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
